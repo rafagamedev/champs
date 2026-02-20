1b37d1ce0a898605a05824f6b0f2777294dcfe6a
rafagamedev <rafagamedev@gmail.com>
2026-02-20 20:48:52 +0000

    Alteração do index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 function Home(){
-    return <h1>Saudades do meu bissim ... 😓
-        Volta logo!
+    return <h1>KD meu bissim feio?
+    
+    <img src="https://assojuris.org.br/wp-content/uploads/2022/07/Sem-titulo-23.jpg" style={{ maxWidth: "100%", height: "auto" }} /> 
+    
+    <iframe
+        src="https://www.tiktok.com/embed/7268400636662729989"
+        width="325"
+        height="575"
+        style={{ border: "none", maxWidth: "100%" }}
+        allowFullScreen
+        title="TikTok Video"
+      ></iframe>
+      
     </h1>
+    
 }
 
 export default Home;
