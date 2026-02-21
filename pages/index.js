401b284e367488aaac9988117b00be036a22c3d3
rafagamedev <rafagamedev@gmail.com>
2026-02-21 21:23:34 +0000

    Adicionado arquivos e configuração para formatação `prettier`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-function Home(){
-    return <h1>KD meu bissim feio?
-    
-    <img src="https://assojuris.org.br/wp-content/uploads/2022/07/Sem-titulo-23.jpg" style={{ maxWidth: "100%", height: "auto" }} /> 
-    
-    <iframe
+function Home() {
+  return (
+    <h1>
+      KD meu bissim feio?
+      <img
+        src="https://assojuris.org.br/wp-content/uploads/2022/07/Sem-titulo-23.jpg"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+      <iframe
         src="https://www.tiktok.com/embed/7268400636662729989"
         width="325"
         height="575"
@@ -11,9 +14,8 @@ function Home(){
         allowFullScreen
         title="TikTok Video"
       ></iframe>
-      
     </h1>
-    
+  );
 }
 
 export default Home;
