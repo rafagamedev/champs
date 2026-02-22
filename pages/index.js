328cc30aaa761ad85676b6e1c5f4306aa99530a9
rafagamedev <rafagamedev@gmail.com>
2026-02-22 04:38:25 +0000

    index alterado

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 function Home() {
   return (
     <h1>
-      KD meu bissim feio?
+      Queria meu bissim...
       <img
-        src="https://assojuris.org.br/wp-content/uploads/2022/07/Sem-titulo-23.jpg"
+        src="https://chatgpt.com/s/m_699a87fac7a48191b81ceb98e79188bc"
         style={{ maxWidth: "100%", height: "auto" }}
       />
-      <iframe
-        src="https://www.tiktok.com/embed/7268400636662729989"
-        width="325"
-        height="575"
-        style={{ border: "none", maxWidth: "100%" }}
-        allowFullScreen
-        title="TikTok Video"
-      ></iframe>
     </h1>
   );
 }
